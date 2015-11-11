68f04c465da384ff18b55dc135fed4e948c45286
joshhornby <hello@joshhornby.co.uk>
2015-11-11 23:14:15 +0000

    Added new prompt
    Based on https://gist.github.com/agnoster/3712874

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,150 +1,153 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
-# Set RHS prompt for git repositories
-DIFF_SYMBOL="•"
-CHECK_SYMBOL="✔"
-CROSS_SYMBOL="✖"
-GIT_PROMPT_SYMBOL=""
-GIT_PROMPT_PREFIX="%{$fg[violet]%}%B(%b%{$reset_color%}"
-GIT_PROMPT_SUFFIX="%{$fg[violet]%}%B)%b%{$reset_color%}"
-GIT_PROMPT_AHEAD="%{$fg[teal]%}%B+NUM%b%{$reset_color%}"
-GIT_PROMPT_BEHIND="%{$fg[orange]%}%B-NUM%b%{$reset_color%}"
-GIT_PROMPT_MERGING="%{$fg[cyan]%}%Bx%b%{$reset_color%}"
-GIT_PROMPT_UNTRACKED="%{$fg[yellow]%}%B$CROSS_SYMBOL%b%{$reset_color%}"
-GIT_PROMPT_MODIFIED="%{$fg[red]%}%B$CROSS_SYMBOL%b%{$reset_color%}"
-GIT_PROMPT_STAGED="%{$fg[green]%}%B$CHECK_SYMBOL%b%{$reset_color%}"
-GIT_PROMPT_DETACHED="%{$fg[neon]%}%B!%b%{$reset_color%}"
+# vim:ft=zsh ts=2 sw=2 sts=2
+#
+# agnoster's Theme - https://gist.github.com/3712874
+# A Powerline-inspired theme for ZSH
+#
+# # README
+#
+# In order for this theme to render correctly, you will need a
+# [Powerline-patched font](https://gist.github.com/1595572).
+#
+# In addition, I recommend the
+# [Solarized theme](https://github.com/altercation/solarized/) and, if you're
+# using it on Mac OS X, [iTerm 2](http://www.iterm2.com/) over Terminal.app -
+# it has significantly better color fidelity.
+#
+# # Goals
+#
+# The aim of this theme is to only show you *relevant* information. Like most
+# prompts, it will only show git information when in a git working directory.
+# However, it goes a step further: everything from the current user and
+# hostname to whether the last call exited with an error to whether background
+# jobs are running in this shell will all be displayed automatically when
+# appropriate.
 
+### Segment drawing
+# A few utility functions to make it easy and re-usable to draw segmented prompts
 
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
+CURRENT_BG='NONE'
+PRIMARY_FG=black
 
-parse_git_detached() {
-    if ! git symbolic-ref HEAD >/dev/null 2>&1; then
-        echo "${GIT_PROMPT_DETACHED}"
+# Characters
+SEGMENT_SEPARATOR="\ue0b0"
+PLUSMINUS="\u00b1"
+BRANCH="\ue0a0"
+DETACHED="\u27a6"
+CROSS="\u2718"
+LIGHTNING="\u26a1"
+GEAR="\u2699"
+
+# Begin a segment
+# Takes two arguments, background and foreground. Both can be omitted,
+# rendering default background/foreground.
+prompt_segment() {
+  local bg fg
+  [[ -n $1 ]] && bg="%K{$1}" || bg="%k"
+  [[ -n $2 ]] && fg="%F{$2}" || fg="%f"
+  if [[ $CURRENT_BG != 'NONE' && $1 != $CURRENT_BG ]]; then
+    print -n "%{$bg%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR%{$fg%}"
+  else
+    print -n "%{$bg%}%{$fg%}"
+  fi
+  CURRENT_BG=$1
+  [[ -n $3 ]] && print -n $3
+}
+
+# End the prompt, closing any open segments
+prompt_end() {
+  if [[ -n $CURRENT_BG ]]; then
+    print -n "%{%k%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR"
+  else
+    print -n "%{%k%}"
+  fi
+  print -n "%{%f%}"
+  CURRENT_BG=''
+}
+
+### Prompt components
+# Each component will draw itself, and hide itself if no information needs to be shown
+
+# Context: user@hostname (who am I and where am I)
+prompt_context() {
+  local user=`whoami`
+
+  if [[ "$user" != "$DEFAULT_USER" || -n "$SSH_CONNECTION" ]]; then
+    prompt_segment $PRIMARY_FG default " %(!.%{%F{yellow}%}.)$user@%m "
+  fi
+}
+
+# Git: branch/detached head, dirty status
+prompt_git() {
+  local color ref
+  is_dirty() {
+    test -n "$(git status --porcelain --ignore-submodules)"
+  }
+  ref="$vcs_info_msg_0_"
+  if [[ -n "$ref" ]]; then
+    if is_dirty; then
+      color=yellow
+      ref="${ref} $PLUSMINUS"
+    else
+      color=green
+      ref="${ref} "
     fi
-}
-
-# Show Git branch/tag, or name-rev if on detached head
-parse_git_branch() {
-  (git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
-}
-
-parse_git_state() {
-  # Compose this value via multiple conditional appends.
-  local GIT_STATE=""
-
-  local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
-  if [ "$NUM_AHEAD" -gt 0 ]; then
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
-  fi
-
-  local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
-  if [ "$NUM_BEHIND" -gt 0 ]; then
-      if [[ -n $GIT_STATE ]]; then
-          GIT_STATE="$GIT_STATE "
-      fi
-      GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
-  fi
-
-  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
-    if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
-        if [[ -n $GIT_STATE ]]; then
-            GIT_STATE="$GIT_STATE "
-        fi
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
-  fi
-
-  if [[ -n $(git ls-files --other --exclude-standard :/ 2> /dev/null) ]]; then
-    GIT_DIFF=$GIT_PROMPT_UNTRACKED
-  fi
-
-  if ! git diff --quiet 2> /dev/null; then
-    GIT_DIFF=$GIT_DIFF$GIT_PROMPT_MODIFIED
-  fi
-
-  if ! git diff --cached --quiet 2> /dev/null; then
-    GIT_DIFF=$GIT_DIFF$GIT_PROMPT_STAGED
-  fi
-
-  if [[ -n $GIT_STATE && -n $GIT_DIFF ]]; then
-    GIT_STATE="$GIT_STATE "
-  fi
-    GIT_STATE="$GIT_STATE$GIT_DIFF"
-
-  if [[ -n $GIT_STATE ]]; then
-    echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"
-  fi
-}
-
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-#echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
-  echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
-# Credit: https://github.com/jackfranklin/dotfiles/blob/6e150a68eb1970789b3a8a39e584294880e85650/zsh/prompt#L42
-
-# Current directory, truncated to 3 path elements (or 4 when one of them is "~")
-# The number of elements to keep can be specified as ${1}
-directory_name() {
-  local sub=${1}
-  if [[ "${sub}" == "" ]]; then
-      sub=3
-  fi
-  local len="$(expr ${sub} + 1)"
-  local full="$(print -P "%d")"
-  local relfull="$(print -P "%~")"
-  local shorter="$(print -P "%${len}~")"
-  local current="$(print -P "%${len}(~:.../:)%${sub}~")"
-  local last="$(print -P "%1~")"
-
-  # Longer path for '~/...'
-  if [[ "${shorter}" == \~/* ]]; then
-      current=${shorter}
-  fi
-
-  local truncated="$(echo "${current%/*}/")"
-
-  # Handle special case of directory '/' or '~something'
-  if [[ "${truncated}" == "/" || "${relfull[1,-2]}" != */* ]]; then
-      truncated=""
-  fi
-
-  # Handle special case of last being '/...' one directory down
-  if [[ "${full[2,-1]}" != "" && "${full[2,-1]}" != */* ]]; then
-      truncated="/"
-      last=${last[2,-1]} # take substring
-  fi
-
-  echo "%{$fg[red]%}${truncated}%{$fg[orange]%}%B${last}%b%{$reset_color%}"
-}
-
-# If inside a Git repository, print its branch and state
-RPR_SHOW_GIT=true # Set to false to disable git status in rhs prompt
-function git_prompt_string() {
-    if [[ "${RPR_SHOW_GIT}" == "true" ]]; then
-        local git_where="$(parse_git_branch)"
-        local git_detached="$(parse_git_detached)"
-        [ -n "$git_where" ] && echo " $GIT_PROMPT_SYMBOL$(parse_git_state)$GIT_PROMPT_PREFIX%{$fg[magenta]%}%B${git_where#(refs/heads/|tags/)}%b$git_detached$GIT_PROMPT_SUFFIX"
+    if [[ "${ref/.../}" == "$ref" ]]; then
+      ref="$BRANCH $ref"
+    else
+      ref="$DETACHED ${ref/.../}"
     fi
+    prompt_segment $color $PRIMARY_FG
+    print -Pn " $ref"
+  fi
 }
 
-export PROMPT=$'\nin $(directory_name) $(git_prompt_string)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[red]%}%{$reset_color%}"
+# Dir: current working directory
+prompt_dir() {
+  prompt_segment blue $PRIMARY_FG ' %~ '
 }
 
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
+# Status:
+# - was there an error
+# - am I root
+# - are there background jobs?
+prompt_status() {
+  local symbols
+  symbols=()
+  [[ $RETVAL -ne 0 ]] && symbols+="%{%F{red}%}$CROSS"
+  [[ $UID -eq 0 ]] && symbols+="%{%F{yellow}%}$LIGHTNING"
+  [[ $(jobs -l | wc -l) -gt 0 ]] && symbols+="%{%F{cyan}%}$GEAR"
+
+  [[ -n "$symbols" ]] && prompt_segment $PRIMARY_FG default " $symbols "
 }
+
+## Main prompt
+prompt_agnoster_main() {
+  RETVAL=$?
+  CURRENT_BG='NONE'
+  prompt_status
+  prompt_context
+  prompt_dir
+  prompt_git
+  prompt_end
+}
+
+prompt_agnoster_precmd() {
+  vcs_info
+  PROMPT='%{%f%b%k%}$(prompt_agnoster_main) '
+}
+
+prompt_agnoster_setup() {
+  autoload -Uz add-zsh-hook
+  autoload -Uz vcs_info
+
+  prompt_opts=(cr subst percent)
+
+  add-zsh-hook precmd prompt_agnoster_precmd
+
+  zstyle ':vcs_info:*' enable git
+  zstyle ':vcs_info:*' check-for-changes false
+  zstyle ':vcs_info:git*' formats '%b'
+  zstyle ':vcs_info:git*' actionformats '%b (%a)'
+}
+
+prompt_agnoster_setup "$@"
